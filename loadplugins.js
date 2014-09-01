75b5fbc5c9cc186bb061329cf9292713a5e1eb36
ljv5555 <jjverderber@gmail.com>
2014-09-01 15:32:57 -0500

    Update loadplugins.js

--- loadplugins.js
+++ loadplugins.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(function($){
   var buttontemplate = _.template('<div><button type="button" class="button_b button_b_<%=_.now()+"" %>" onclick="<%=onclick %>" ><%=text %></button>');
   var d1 = $('<div/>');
   d1.html(buttontemplate({"onclick":"alert(1);","text":"sometext"}));

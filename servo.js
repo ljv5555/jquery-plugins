73d5017e42db27e17bc5c135fdc610f120d2fe00
ljv5555 <jjverderber@gmail.com>
2014-09-07 14:46:12 -0500

    Update servo.js

--- servo.js
+++ servo.js
@@ -1,4 +1,13 @@
 // servo.js
 // utilities and waveforms to control servos connected through headphone port
 
-jQuery(function($){$.fn.servo=function(p){console.log('p='+p+);console.log('this=');console.log(this);}; || '');});
+jQuery(function($){
+  
+  
+  
+  $.fn.servo=function(p){console.log('p='+p+);console.log('this=');console.log(this);};
+  
+  
+  
+  
+});
